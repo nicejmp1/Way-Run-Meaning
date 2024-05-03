465aa2677a0bb29979aa45bafdd038ad550abeb8
박정민 <a01063363854@gmail.com>
2024-05-03 09:45:51 +0900

    2024

---
+++ coding/assets/js/gsap.js
@@ -0,0 +1,4 @@
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollToPlugin.min.js"></script>
