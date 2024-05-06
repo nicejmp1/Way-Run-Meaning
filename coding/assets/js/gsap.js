736b9f4f18e131008c9535964fc4acc64605d7ab
박정민 <a01063363854@gmail.com>
2024-05-06 20:48:25 +0900

    gg

--- coding/assets/js/gsap.js
+++ coding/assets/js/gsap.js
@@ -1,4 +1,19 @@
-<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollToPlugin.min.js"></script>
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".main__crew__rank",
+        start: "top top", // 시작 지점: .container가 뷰포트 상단에 닿을 때
+        end: "+=1000", // 종료 지점: 시작점으로부터 3000px 아래에서
+        scrub: true,
+        pin: ".main__crew", // .main__crew 요소를 고정
+        anticipatePin: 1,
+        markers: true // 개발 시 위치 확인을 위한 마커 표시
+    }
+})
+    .from(".crew__rank .crew1", { autoAlpha: 0, borderRadius: 200 }, 'a')
+    .from(".crew__rank .crew2", { autoAlpha: 0, borderRadius: 200 }, '+=2')
+    .from(".crew__rank .crew3", { autoAlpha: 0, borderRadius: 200 }, 'a')
+    .from(".crew__rank .crew4", { autoAlpha: 0, borderRadius: 200 }, 'b');
+
